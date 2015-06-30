ecafc7eb3fe898516ae43ef095e1ed6e91aa4531
chriscommet <chriscomet210@gmail.com>
2015-06-30 09:43:18 -0400

    Updated the calendarjs file

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,110 +1,129 @@
-var jobIDNumber = 1; 
-var eventCollects    = new Array();
-              
-function validateInputData(){
-    
-        if($('#jobID').val() == "" ) {
-                prompt("JobID wasn't created... You can create the NewJob by clicking the Add Booking.");
-                return false;
-            }
-        if($('#startDate').val() == ""){
-                prompt("startDate wasn't created...");
-                return false;
-            } 
-        if($('#teamSize').val() == ""){
-                prompt("teamSize wasn't created...");
-                return false;
-            }  
-        if($('#address').val() == ""){
-                prompt("address wasn't created...");
-                return false;
-            }  
-        if($('#city').val() == "" ){
-                prompt("city wasn't created...");
-                return false;
-            }  
-        if($('#country').val() == ""){
-                prompt("country wasn't created...");
-                return false;
-            }  
-        if($('#customerName').val() == ""){
-                prompt("customerName wasn't created...");
-                return false;
-            }  
-        if($('#phone').val() == ""){                                                                               
-                prompt("phone wasn't created...");
-                return false;
-            }  
-        if($('#source').val() == ""){
-                prompt("SourceData wasn't referenced...");
-                return false;
-            }  
-        return true;
-    };     
+var jobIDNumber = 1;  
+        var registeredId;                                
+        var eventCollects    = new Array();
+        
+        function initialInputData(){
                 
-function set_associated_data(){    
-    
-        alert("New JobID will be created automatically.");    
+                if($('#jobID').val() == "" ) {
+                        alert("JobID wasn't created... You can create the NewJob by clicking the Add Booking.");
+                        return false;
+                    }  
+                if($('#startDate').val() == ""){
+                    alert("StartDate wasn't created...");
+                    return false;
+                }
+                if($('#teamSize').val() == ""){
+                        alert("TeamSize wasn't created...");
+                        return false;
+                    }  
+                if($('#address').val() == ""){
+                        alert("Address wasn't created...");
+                        return false;
+                    }  
+                if($('#city').val() == "" ){
+                        alert("City wasn't created...");
+                        return false;
+                    }  
+                if($('#country').val() == ""){
+                        alert("Country wasn't created...");
+                        return false;
+                    }  
+                if($('#customerName').val() == ""){
+                        alert("CustomerName wasn't created...");
+                        return false;
+                    }  
+                if($('#phone').val() == ""){                                                                               
+                        alert("Phone wasn't created...");
+                        return false;
+                    }  
+                if($('#source').val() == ""){
+                        alert("SourceData wasn't referenced...");
+                        return false;
+                    }  
+                return true;
+            };     
                         
-        document.getElementById("jobID").value = jobIDNumber;    
-        document.getElementById("startDate").value = "";
-        document.getElementById("teamSize").value = "";
-        document.getElementById("address").value = "";
-        document.getElementById("city").value = "";
-        document.getElementById("country").value = "";
-        document.getElementById("customerName").value = "";
-        document.getElementById("phone").value = "";
-};            
+        function set_associated_data(){    
+            
+                document.getElementById("jobID").value = jobIDNumber;
+                alert("New JobID was created automatically.");          
+                registeredId = jobIDNumber;
+                document.getElementById("teamSize").value = "";
+                document.getElementById("address").value = "";
+                document.getElementById("city").value = "";
+                document.getElementById("country").value = "";
+                document.getElementById("customerName").value = "";
+                document.getElementById("phone").value = "";
+        }; 
+        
+        function  insert_eventlist(){
+            
+        }          
+        
+        $(document).ready(function(){    
+            
+            $('#calendar').fullCalendar({     
+                
+                theme: true,
+                editable: true,    
+                selectable: true,
+                selectHelper: true,     
+                editable: true,
+                
+                header: {
+                    left: 'prev',
+                    center: 'title', 
+                    right: 'next'
+                    },
+                <!-- --> 
+                eventClick: function (calEvent, jsEvent, view) {
+                    $('#calendar').fullCalendar('removeEvents', function (calEvent) {
+                        return true;
+                    });
+                },
 
-$(document).ready(function(){
-    
-    $('#calendar').fullCalendar({     
-        
-        theme: true,
-        editable: true,    
-        selectable: true,
-        selectHelper: true,     
-        editable: true,
-        
-        header: {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-            },
-        events: eventCollects,
-        dayClick: function(date, jsEvent, view) {
-            document.getElementById("startDate").value = date.format();
-        }
-    });
-        
-    $('#savingCommand').click(function(){  
-        
-            if(validateInputData()){
+                dayClick: function(date, jsEvent, view) {
+
+                    document.getElementById("startDate").value = date.format();
+
+                }
+            });
                 
-                var job_id = $('#jobID').val();    
-                var team_size = $('#teamSize').val();
-                
-                $("ul").append("<li>Job" + job_id +" - " + team_size + "People" + "<a href='javascript:void(0);' class='remove'>               Remove</a></li>"); 
-                $("ul li").addClass('job-log');
-                
-                jobIDNumber++; 
-                
-                var newEvent = new Object();
-                newEvent.title = $('#customerName').val();                                                     
-                newEvent.start = $('#startDate').val();
-                newEvent.allDay = false;
-                
-                $('#calendar').fullCalendar( 'renderEvent', newEvent );  
-            }
+            $('#savingCommand').click(function(){  
+                    if(initialInputData()){    
+                        
+                        var jobId = $('#jobID').val();    
+                        var teamSize = $('#teamSize').val();    
+                        var startDate = $('#startDate').val();    
+                        var address = $('#address').val();    
+                        var city = $('#city').val();    
+                        var country = $('#country').val();    
+                        var customerNumber = $('#customerNumber').val();    
+                        var phone = $('#phone').val();    
+                        var source = $('#source').val();
+                        
+                        insert_eventlist();
+                        $("ul").append("<li>Job" + jobId +" - " + team_size + "People" + "</li>"); 
+                        $("ul li").addClass('job-log'); 
+                            
+                        var newEvent = new Object();
+                        newEvent.id = jobId;
+                        newEvent.title = $('#customerName').val();                         
+                        newEvent.start = $('#startDate').val();
+                        newEvent.allDay = false;                            
+                        $('#calendar').fullCalendar( 'renderEvent', newEvent );   
+                        jobIDNumber++;  
+                    }   
+                           
+            });    
             
-            var sepEvent = [$('#customerName').val(), $('#startDate').val()];    
-            
-    });    
-        
-    $(document).on("click", "a.remove" , function() {
-        
-            $(this).parent().remove();
-            
-    });                   
-      
-});  
+            $('#editBooking').click(function(){  
+                var editEventId = prompt("Enter your JobId you want to edit. : ", "your name here");
+                alert(editEventId );                           
+            }); 
+                
+            $(document).on("click", "a.remove" , function() {    
+                    $(this).parent().remove()  
+            });                   
+              
+        }); 
