9d2e54762d23c36a20ac7390f702daba72586cc2
dmitry <kosenkodmitryv@gmail.com>
2015-08-22 16:21:05 +0300

    Refactoring. Brand name.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20150821160230) do
-  
+
   create_table "bookings", force: :cascade do |t|
     t.date    "date"
     t.integer "team_size"
